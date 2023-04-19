1948728f1664c5a2143d80e18a01a5f95758ffde
david hackerman <david.verheyden@student.ucll.be>
2023-04-19 21:56:01 +0200

    nu de BeTeRe map

--- platformer_VMC/Models/object_enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ObjectEnum(Enum):
-    PLAYER = "../assets/MainCharacters/"
-    FIRE = "../assets/Traps/Fire/"
-    BLOCK = "../assets/Terain/"
--- platformer_VMC/tutorial.py
+++
@@ -1,7 +0,0 @@
-import os
-import random
-import math
-import pygame
-from os import listdir
-from os.path import isfile, join
-
